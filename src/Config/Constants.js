edbf2b3723cf3426e5b9e05906c8ba8a52720502
Sandeep Kumar Singh <itissandeep98@gmail.com>
2022-01-27 14:35:55 +0530

    faucet update

--- src/Config/Constants.js
+++ src/Config/Constants.js
@@ -1,3 +1,3 @@
-export const FaucetAddress = "0x9a174D25601A9162043B68867ecc955DF84832Cb";
+export const FaucetAddress = "0x6b7913A89E0DAEC0056A844298957A3A1b2CeD5b";
 
 export const TokenAddress = "0x602f925c008673a9cfb4be133e5359a52eec18a1";
